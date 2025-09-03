b99b7e56c7ed3c989cc80c1acbb9e32ec38592da
Shouqiang Li <shouqiangli@Shouqiangs-MacBook-Pro.local>
2025-09-03 15:38:28 +1000

    implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,21 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+async function arnieHttpGet(url) {
+  const result = await httpGet(url);
+  if (result.status === 200) {
+    return {
+      "Arnie Quote": JSON.parse(result.body).message,
+    };
+  }
+
+  return {
+    FAILURE: "Your request has been terminated",
+  };
+}
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = await Promise.all(urls.map((url) => arnieHttpGet(url)));
+  return results;
 };
 
 module.exports = {
